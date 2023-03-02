c6e711a5099617d6c27ad23e71c68c74ed81174d
santhoshagganur <118893483+santhoshagganur@users.noreply.github.com>
2023-03-02 20:27:44 +0530

    search test cases

--- src/components/PasswordBox/index.js
+++ src/components/PasswordBox/index.js
@@ -1,7 +1,7 @@
 import './index.css'
 
 const PasswordBox = props => {
-  const {userPasswords, deleteItemFromList, isChecked, passwordsList} = props
+  const {userPasswords, deleteItemFromList, isChecked} = props
   const {name, password, website, id} = userPasswords
 
   const deletePassword = () => {
@@ -18,40 +18,32 @@ const PasswordBox = props => {
     />
   )
 
-  const userInterface =
-    passwordsList.length === 0 ? (
-      <>
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/no-passwords-img.png"
-          alt="no passwords"
-          className="no-passwords"
-        />
-      </>
-    ) : (
-      <>
-        <div className="user-profile-pic">
-          <h1> {name[0]} </h1>
-        </div>
-        <div>
-          <p className="website-name"> {website} </p>
-          <p className="user-name"> {name} </p>
-          {result}
-        </div>
-        <div className="delete-container">
-          <button
-            type="button"
-            className="delete-button"
-            onClick={deletePassword}
-          >
-            <img
-              src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png"
-              alt="delete"
-              className="delete-icon"
-            />
-          </button>
-        </div>
-      </>
-    )
+  const userInterface = (
+    <>
+      <div className="user-profile-pic">
+        <h1> {name[0]} </h1>
+      </div>
+      <div>
+        <p className="website-name"> {website} </p>
+        <p className="user-name"> {name} </p>
+        {result}
+      </div>
+      <div className="delete-container">
+        <button
+          type="button"
+          className="delete-button"
+          onClick={deletePassword}
+          data-testid="delete"
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png"
+            alt="delete"
+            className="delete-icon"
+          />
+        </button>
+      </div>
+    </>
+  )
 
   return <li className="each-password-container">{userInterface}</li>
 }
